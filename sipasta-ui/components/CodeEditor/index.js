23596eb96511a0876cd05410c385fe74073efd09
Haydar <alkaf.haydar@gmail.com>
2022-11-09 19:35:05 +0700

    init: real FE

---
+++ sipasta-ui/components/CodeEditor/index.js
@@ -0,0 +1,43 @@
+import CodeMirror from '@uiw/react-codemirror';
+import { markdown, markdownLanguage } from '@codemirror/lang-markdown';
+import { languages } from '@codemirror/language-data';
+import { githubLight, githubDark } from '@uiw/codemirror-theme-github';
+
+const code = `## Title
+
+\`\`\`jsx
+function Demo() {
+  return <div>demo</div>
+}
+\`\`\`
+
+\`\`\`bash
+# Not dependent on uiw.
+npm install @codemirror/lang-markdown --save
+npm install @codemirror/language-data --save
+\`\`\`
+
+[weisit ulr](https://uiwjs.github.io/react-codemirror/)
+
+\`\`\`go
+package main
+import "fmt"
+func main() {
+  fmt.Println("Hello, 世界")
+}
+\`\`\`
+`;
+
+export default function CodeEditor() {
+  return (
+    <CodeMirror
+      value={code}
+      height="200px"
+      theme={githubLight}
+      extensions={[markdown({ base: markdownLanguage, codeLanguages: languages })]}
+      onChange={(value, viewUpdate) => {
+        console.log('value:', value);
+      }}
+    />
+  )
+}
