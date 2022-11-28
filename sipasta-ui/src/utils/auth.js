c7a12c423dad3acac8219876c663a248eadcf8bd
Haydar <alkaf.haydar@gmail.com>
2022-11-28 22:00:42 +0700

    FE: auth forgot to save

--- sipasta-ui/src/utils/auth.js
+++ sipasta-ui/src/utils/auth.js
@@ -2,9 +2,9 @@ import { initializeApp } from "firebase/app";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    projectId: process.env.PROJECT_ID,
-    authDomain: process.env.AUTH_DOMAIN,
-    apiKey: process.env.API_KEY,
+    projectId: `${process.env.PROJECT_ID}`,
+    authDomain: `${process.env.AUTH_DOMAIN}`,
+    apiKey: `${process.env.API_KEY}`,
 };
 
 const app = initializeApp(firebaseConfig);
